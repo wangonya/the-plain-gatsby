2c424a73a8b200086464d459d293c1d68de05c88
wangonya <kwangonya@gmail.com>
2019-05-21 16:30:56 +0300

    edit footer

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Footer = () => (
+  <div className="footer">
+    <span className="block">
+      <small>
+        Built with {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a> and{` `}
+        <a href="#">The Plain Gatsby starter</a>
+      </small>
+    </span>
+  </div>
+)
+
+export default Footer
--- src/layouts/default.js
+++ src/layouts/default.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from ".././components/header"
+import Footer from ".././components/footer"
 import "../styles/styles.scss"
 
 const DefaultLayout = ({ children }) => (
@@ -26,21 +27,10 @@ const DefaultLayout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div>
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
         </div>
+        <Footer />
       </>
     )}
   />
