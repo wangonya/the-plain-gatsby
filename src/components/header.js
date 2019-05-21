ea192994b905ee36e34fb8b8028b2b3139ce8d88
wangonya <kwangonya@gmail.com>
2019-05-21 15:56:26 +0300

    add logo header

--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,13 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className="logo">
+    <div>
+      {window.location.pathname == "/" ? (
+        <Link to="/about/" />
+      ) : (
+        <Link to="/" />
+      )}
     </div>
   </header>
 )
