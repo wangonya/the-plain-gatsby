85cacda81ba46d7ba2c51a6717306fbce7e64a4b
wangonya <kwangonya@gmail.com>
2019-05-24 19:43:51 +0300

    configure header

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,27 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Location } from "@reach/router"
 
 const Header = ({ siteTitle }) => (
   <header className="logo">
-    <div>
-      {window.location.pathname == "/" ? (
-        <Link to="/about/" />
-      ) : (
-        <Link to="/" />
-      )}
-    </div>
+    <Location>
+      {({ location }) => {
+        return location.pathname == "/" ? (
+          <div>
+            <Link to="/about/">
+              <img src=".././images/gatsby-icon.png" />
+            </Link>
+            <span className="logo-prompt code">About the Author</span>
+          </div>
+        ) : (
+          <div>
+            <Link to="/" />
+            <span className="logo-prompt code">Back Home</span>
+          </div>
+        )
+      }}
+    </Location>
   </header>
 )
 
